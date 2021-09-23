952df273e9afe1cc613a101e301dc12b601695bc
EC2E\Leo <bruantleo@gmail.com>
2021-09-23 17:01:35 +0200

    Added simulation mode

--- script.js
+++ script.js
@@ -1,153 +1,177 @@
-function loadProcesses(processIndex){
-	// Substract the quantum value to the current process
+function loadProcessesRoundRobin(processIndex){
+  // Substract the quantum value to the current process
+  
+  for (let i = 0; i < resultColumns.quantumPerRound; i++){
+    if (processes[processIndex] > 0){
+      processes[processIndex]--
+      totalTime++
+    }
+  }
+  
+  // If Processes are still loading
+  
+  if(processes.length !== 0){
+    
+    // Remove process if it is loaded
+    
+    if (processes[processIndex] === 0){
+      processes.splice(processIndex, 1)
+      
+      // Go back to the first process if we are at the end
+      
+      if (processIndex === processes.length){
+        processIndex = 0
+      }
+    }
+    
+    else{
+      // Go back to the first process if we are at the end
+      
+      if (processIndex === processes.length - 1){
+        processIndex = 0
+      }
+      
+      // Or keep going
+      
+      else{
+        processIndex++
+      }
+    }
+    
+    // Add context change duration to total time
+    
+    totalTime += resultColumns.contextChangeDuration
+    
+    loadProcessesRoundRobin(processIndex)
+  }
+}
 
-    for (let i = 0; i < resultColumns.quantumPerRound; i++){
-        if (processes[processIndex] > 0){
-            processes[processIndex]--
-			totalTime++
-		}
-	}
+function loadProcessesSmallestFirst(){
+  processes.forEach(process => {
+    for (let i = 0; i < process; i++){
+      if (process > 0){
+        process--
+        totalTime++
 
-    // If Processes are still loading
-
-	if(processes.length !== 0){
-
-        // Remove process if it is loaded
-
-        if (processes[processIndex] === 0){
-            processes.splice(processIndex, 1)
-
-            // Go back to the first process if we are at the end
-
-            if (processIndex === processes.length){
-                processIndex = 0
-            }
-		}
-        
-        else{
-            // Go back to the first process if we are at the end
-
-            if (processIndex === processes.length - 1){
-                processIndex = 0
-            }
-
-            // Or keep going
-
-            else{
-                processIndex++
-            }
+        if(i % resultColumns.quantumPerRound === 0){
+          totalTime += resultColumns.contextChangeDuration
         }
-
-        // Add context change duration to total time
-
-        totalTime += resultColumns.contextChangeDuration
-
-        loadProcesses(processIndex)
-	}
+      }
+    }
+  })
 }
 
 function createProcesses(processesDurationMin, processesDurationMax){
-    let processesDuration = 0
-
-    // Randomly create processes
-
-    for (let i = 0; i < resultColumns.processesNumber; i++){
-        let duration = Math.round(Math.random() * (processesDurationMax - processesDurationMin) + processesDurationMin)
-
-        processes.push(duration)
-        processesDuration += duration
-	}
-
-    // Get process average time
-
-    resultColumns.averageProcessDuration = (processesDuration / resultColumns.processesNumber).toFixed(2)
+  let processesDuration = 0
+  
+  // Randomly create processes
+  
+  for (let i = 0; i < resultColumns.processesNumber; i++){
+    let duration = Math.round(Math.random() * (processesDurationMax - processesDurationMin) + processesDurationMin)
+    
+    processes.push(duration)
+    processesDuration += duration
+  }
+  
+  // Get process average time
+  
+  resultColumns.averageProcessDuration = (processesDuration / resultColumns.processesNumber).toFixed(2)
 }
 
 function reset() {
-    totalTime = 0
-    processes = []
+  totalTime = 0
+  processes = []
 }
 
 function loadResultsTable() {
-    let resultsTable = document.querySelector('#results table tbody')
-    let row = document.createElement('tr')
-
-    // Check if row number is even
-
-    if (resultsTable.childElementCount % 2 === 0) {
-        var even = true
-    }
-
-    // Add row number
-
-    let column = document.createElement('td')
-
-    column.innerText = resultsTable.childElementCount + 1
+  let resultsTable = document.querySelector('#results table tbody')
+  let row = document.createElement('tr')
+  
+  // Check if row number is even
+  
+  var background = ''
+  
+  if (resultsTable.childElementCount % 2 === 0) {
+    background += ' bg-warmGray-100'
+  }
+  
+  // Add row number
+  
+  let column = document.createElement('td')
+  
+  column.innerText = resultsTable.childElementCount + 1
+  column.className = 'border border-sky-600 p-2'
+  
+  // Set background
+  
+  column.className += background
+  
+  row.append(column)
+  
+  // For each column
+  
+  Object.values(resultColumns).forEach(data => {
+    
+    // Fill data
+    
+    column = document.createElement('td')
+    
+    column.innerText = data
     column.className = 'border border-sky-600 p-2'
-
+    
     // Set background
-
-    if (even) {
-        column.className += ' bg-sky-100'
-    }
-
+    
+    column.className += background
+    
+    // Add data to row
+    
     row.append(column)
-
-    // For each column
-
-    Object.values(resultColumns).forEach(data => {
-
-        // Fill data
-
-        column = document.createElement('td')
-
-        column.innerText = data
-        column.className = 'border border-sky-600 p-2'
-
-        // Set background
-
-        if (even) {
-            column.className += ' bg-sky-100'
-        }
-
-        // Add data to row
-
-        row.append(column)
-    })
-
-    // Add row to table
-
-    resultsTable.append(row)
+  })
+  
+  // Add row to table
+  
+  resultsTable.append(row)
 }
 
 // Run simulations after clicking button
 function runSimulations() {
-    // Get data from inputs
+  // Get data from inputs
+  
+  resultColumns.processesNumber = parseInt(document.getElementById('processes-number').value)
+  resultColumns.quantumPerRound = parseInt(document.getElementById('quantum-number').value)
+  resultColumns.contextChangeDuration = parseInt(document.getElementById('context-change-duration').value)
+  let processesDurationMin = parseInt(document.getElementById('processes-load-min').value)
+  let processesDurationMax = parseInt(document.getElementById('processes-load-max').value)
+  
+  let simulationNumber = parseInt(document.getElementById('simulation-number').value)
+  let simulationMode = document.getElementById('simulation-mode').value
+  
+  // If all fields are filled
+  
+  if (!isNaN(resultColumns.processesNumber) && !isNaN(resultColumns.quantumPerRound) && !isNaN(resultColumns.contextChangeDuration) && !isNaN(processesDurationMin) && !isNaN(processesDurationMax) && simulationMode !== 'none'){
+    for (let i = 0; i < simulationNumber; i++){
+      // Reset variables
+      reset()
+      
+      // Create processes
+      createProcesses(processesDurationMin, processesDurationMax)
+      
+      // Load processes round robin
+      if(simulationMode === 'round-robin'){
+        loadProcessesRoundRobin(0)
+        resultColumns.totalTime = totalTime
+      }
 
-    resultColumns.processesNumber = parseInt(document.getElementById('processes-number').value)
-    resultColumns.quantumPerRound = parseInt(document.getElementById('quantum-number').value)
-    resultColumns.contextChangeDuration = parseInt(document.getElementById('context-change-duration').value)
-    let processesDurationMin = parseInt(document.getElementById('processes-load-min').value)
-    let processesDurationMax = parseInt(document.getElementById('processes-load-max').value)
-    let simulationNumber = parseInt(document.getElementById('simulation-number').value)
-
-    // If all fields are filled
-
-    if (!isNaN(resultColumns.processesNumber) && !isNaN(resultColumns.quantumPerRound) && !isNaN(resultColumns.contextChangeDuration) && !isNaN(processesDurationMin) && !isNaN(processesDurationMax)){
-        for (let i = 0; i < simulationNumber; i++){
-            // Reset variables
-            reset()
-
-            // Create processes
-            createProcesses(processesDurationMin, processesDurationMax)
-
-            // Load processes
-            loadProcesses(0)
-
-            // Load data in results table
-            loadResultsTable()
-        }
+      // Load processes smallest first
+      else if(simulationMode === 'smallest-first'){
+        processes.sort()
+        loadProcessesSmallestFirst(0)
+      }
+      
+      // Load data in results table
+      loadResultsTable()
     }
+  }
 }
 
 // Variables
