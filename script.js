2a15c42bc18197d18d629d709f2e54ca899da1de
LeoBruant <bruantleo@gmail.com>
2021-09-25 15:41:42 +0200

    Added pager

--- script.js
+++ script.js
@@ -1,4 +1,70 @@
-function loadProcessesRoundRobin(processIndex){
+// Run simulations
+function runSimulations(mode) {
+    // Get data from inputs
+
+    resultColumns.mode = mode.replace('-', ' ')
+    resultColumns.durationPerRound = parseInt(document.getElementById('round-duration').value)
+    resultColumns.contextChangeDuration = parseInt(document.getElementById('context-change-duration').value)
+
+    let simulationNumber = parseInt(document.getElementById('simulation-number').value)
+
+    // If all fields are filled
+
+    if (!isNaN(resultColumns.durationPerRound) && !isNaN(resultColumns.contextChangeDuration)) {
+        for (let i = 0; i < simulationNumber; i++) {
+            // Reset variables
+            resetVariables()
+
+            // Create processes
+            createProcesses(processesDurationMin, processesDurationMax)
+
+            // Load processes round robin
+            if (mode === 'round-robin') {
+                resultColumns.totalTime = 0
+                loadProcessesRoundRobin(0)
+            }
+
+            // Load processes smallest first
+            else if (mode === 'fastest-first') {
+                processes.sort()
+                resultColumns.totalTime = 0
+                loadProcessesFastestFirst(0)
+            }
+
+            // Load data in pages
+            loadPagesResults(mode)
+        }
+
+        // Update front
+
+        loadResultsTable()
+        updatePager()
+    }
+}
+// Reset variables
+function resetVariables() {
+    totalTime = 0
+    processes = []
+}
+// Randomly generate processes
+function createProcesses(processesDurationMin, processesDurationMax) {
+    let processesDuration = 0
+
+    // Randomly generate processes
+
+    for (let i = 0; i < processesNumber; i++) {
+        let duration = Math.round(Math.random() * (processesDurationMax - processesDurationMin) + processesDurationMin)
+
+        processes.push(duration)
+        processesDuration += duration
+    }
+
+    // Get average processes loading time
+
+    resultColumns.averageProcessDuration = (processesDuration / processesNumber).toFixed(2)
+}
+// Load processes using round robin method
+function loadProcessesRoundRobin(processIndex) {
     // Substract the quantum value to the current process
     
     for (let i = 0; i < resultColumns.durationPerRound; i++){
@@ -44,8 +110,8 @@ function loadProcessesRoundRobin(processIndex){
         loadProcessesRoundRobin(processIndex)
     }
 }
-
-function loadProcessesFastestFirst(){
+// Load processes using fastest first method
+function loadProcessesFastestFirst() {
     processes.forEach((process, index) => {
         let i = 0
         let currentProcess = processes[process]
@@ -70,100 +136,97 @@ function loadProcessesFastestFirst(){
         resultColumns.totalTime += resultColumns.contextChangeDuration
     })
 }
+// Load results in form of pages
+function loadPagesResults(mode){
+    // Add page in none
+    if (pages.length === 0 || pages[pages.length - 1].length === rowsPerTablePage) {
+        pages.push([[]])
 
-function createProcesses(processesDurationMin, processesDurationMax){
-    let processesDuration = 0
-    
-    // Randomly create processes
-    
-    for (let i = 0; i < processesNumber; i++){
-        let duration = Math.round(Math.random() * (processesDurationMax - processesDurationMin) + processesDurationMin)
-        
-        processes.push(duration)
-        processesDuration += duration
+        pagesRowModes.push([[]])
     }
-    
-    // Get process average time
-    
-    resultColumns.averageProcessDuration = (processesDuration / processesNumber).toFixed(2)
-}
 
-function reset() {
-    totalTime = 0
-    processes = []
-}
+    // Add row if last row is full
+    if (pages[pages.length - 1][pages[pages.length - 1].length - 1].length === Object.values(resultColumns).length + 1) {
+        pages[pages.length - 1].push([])
 
-function loadResultsTable(mode) {
-    let resultsTable = document.querySelector('#results table tbody')
-    let row = document.createElement('tr')
-    
+        pagesRowModes[pages.length - 1].push([])
+    }
+
+    // Calculate total elements number
+    totalElementsNumber = 0
+
+    pages.forEach(page => {
+        page.forEach(row => {
+            totalElementsNumber++
+        })
+    })
+
     // Add row number
-    
-    let column = document.createElement('td')
-    
-    column.innerText = resultsTable.childElementCount + 1
-    column.className = 'border-t border-b border-l-2 border-r-2 border-sky-600 p-2 xl:font-semibold border-opacity-40 ' + rowColors[mode]
-    
-    row.append(column)
+    pages[pages.length - 1][pages[pages.length - 1].length - 1].push(totalElementsNumber)
+
+    // Add row mode
+    pagesRowModes[pages.length - 1][pages[pages.length - 1].length - 1] = mode
+
+    // Add data
+    Object.values(resultColumns).forEach(data => {
+        pages[pages.length - 1][pages[pages.length - 1].length - 1].push(data)
+    })
+}
+// Load page results into results table
+function loadResultsTable() {
+    let resultsTable = document.querySelector('#results table tbody')
+    resultsTable.innerHTML = ''
     
     // For each column
     
-    Object.values(resultColumns).forEach(data => {
-        
-        // Fill data
-        
-        column = document.createElement('td')
-        
-        column.innerText = data
-        column.className = 'border border-sky-600 p-2 border-opacity-40 ' + rowColors[mode]
-        
-        // Add data to row
-        
-        row.append(column)
-    })
-    
-    // Add row to table
-    
-    resultsTable.append(row)
-}
+    pages[currentPage - 1].forEach((pageRow, rowIndex) => {
+        let row = document.createElement('tr')
 
-// Run simulations after clicking button
-function runSimulations(mode) {
-    // Get data from inputs
-    
-    resultColumns.mode = mode.replace('-', ' ')
-    resultColumns.durationPerRound = parseInt(document.getElementById('round-duration').value)
-    resultColumns.contextChangeDuration = parseInt(document.getElementById('context-change-duration').value)
-    
-    let simulationNumber = parseInt(document.getElementById('simulation-number').value)
-    
-    // If all fields are filled
-    
-    if (!isNaN(resultColumns.durationPerRound) && !isNaN(resultColumns.contextChangeDuration)){
-        for (let i = 0; i < simulationNumber; i++){
-            // Reset variables
-            reset()
-            
-            // Create processes
-            createProcesses(processesDurationMin, processesDurationMax)
-            
-            // Load processes round robin
-            if(mode === 'round-robin'){
-                resultColumns.totalTime = 0
-                loadProcessesRoundRobin(0)
-            }
-            
-            // Load processes smallest first
-            else if(mode === 'fastest-first'){
-                processes.sort()
-                resultColumns.totalTime = 0
-                loadProcessesFastestFirst(0)
-            }
-            
-            // Load data in results table
-            loadResultsTable(mode)
-        }
+        pageRow.forEach(data => {
+            // Fill data
+
+            column = document.createElement('td')
+
+            column.innerText = data
+            column.className = 'border border-sky-600 p-2 border-opacity-40 ' + rowColors[pagesRowModes[currentPage - 1][rowIndex]]
+
+            // Add data to row
+
+            row.append(column)
+        })
+
+        // Add row to table
+
+        resultsTable.append(row)
+    })
+}
+// Update pager text
+function updatePager(){
+    document.getElementById('current-page').innerText = currentPage + '/' + pages.length
+    document.getElementById('total-elements').innerText = totalElementsNumber + ' éléments'
+    document.getElementsByClassName('pager')[0].style.visibility = 'visible'
+}
+// Go to prev or next page
+function changePage(direction) {
+    if ((direction === -1 && currentPage > 1) || (direction === 1 && pages[currentPage - 1 + direction] !== undefined)){
+        currentPage += direction
+
+        loadResultsTable()
+        updatePager()
     }
+}
+// Go to first or last page
+function pagesEdge(direction){
+    if (direction === -1 && currentPage > 1){
+        currentPage = 1
+    }
+
+    else if (direction === 1 && currentPage !== pages.length){
+        currentPage = pages.length
+    }
+
+    loadResultsTable()
+    updatePager()
 }
 
 // Constants
@@ -171,14 +234,20 @@ function runSimulations(mode) {
 const processesNumber = 10
 const processesDurationMin = 1
 const processesDurationMax = 10
+const rowsPerTablePage = 10
+
 let rowColors = []
 rowColors['round-robin'] = 'bg-cyan-100'
 rowColors['fastest-first'] = 'bg-indigo-100'
 
 // Variables
 
-let processes = []
 let resultColumns = {}
+let processes = []
+let pages = []
+let pagesRowModes = []
+let totalElementsNumber = 0
+let currentPage = 1
 
 // Main
 
